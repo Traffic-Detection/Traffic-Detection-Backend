b9a979d82d7eca8aa40943db81890c1103b9112c
GWuy <buigiahuy1506@gmail.com>
2026-06-03 12:38:32 +0700

    first commit

---
+++ src/main/java/com/gwuy/sba301/trafficdetectionbackend/TrafficDetectionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gwuy.sba301.trafficdetectionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficDetectionBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficDetectionBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gwuy/sba301/trafficdetectionbackend/TrafficDetectionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwuy.sba301.trafficdetectionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficDetectionBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
